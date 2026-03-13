6da02473e20359f4f29ce823ff44261cf30ea585
diegodlf74 <diegodlf74@gmail.com>
2026-03-13 16:58:24 -0700

    New LevelDemo level

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -2,16 +2,17 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public GameObject player;
-    private Vector3 offset;
-
-    void Start()
-    {
-        offset = transform.position - player.transform.position;
-    }
+    public Transform target;
+    public float distance = 8f;
+    public float smoothSpeed = 5f;
 
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+        if (target == null) return;
+        Vector3 offset = new Vector3(0f, distance, -distance);
+        Vector3 desired = target.position + offset;
+
+        transform.position = Vector3.Lerp(transform.position, desired, smoothSpeed * Time.deltaTime);
+        transform.rotation = Quaternion.Euler(45f, 0f, 0f);
     }
 }
