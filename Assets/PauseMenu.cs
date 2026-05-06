425f60651396dc03f28f91a333ac0e91293d32e4
diegodlf74 <diegodlf74@gmail.com>
2026-05-05 21:50:08 -0700

    Added basic pause menu
    Very basic, especially with fonts, but will be updated

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        print("Returning to menu");
+    }
+
+    public void QuitGame()
+    {
+        print("Quitting game");
+        Application.Quit();
+    }
+}
